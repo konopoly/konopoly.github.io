9b1b0c402f1b05a30facbab3c511f06d6808b4c8
konopoly <kpolyash@gmail.com>
2020-04-27 02:28:16 -0700

    added details for summary

---
+++ index.js
@@ -0,0 +1,40 @@
+const profileImgDiv = document.getElementsByClassName('title-bar__img')[0];
+
+//do stuff after phrase was entered
+const initiate = _ => {
+   alert('Correct Phrase Entered, Initiating');
+} 
+
+//show username and pw for login
+const validate = _ => {
+   const bioDiv = document.getElementsByClassName('bio')[0];
+
+   const phraseInput = document.createElement("Input");
+   phraseInput.setAttribute("id", "phraseInputId");
+   const phraseInputButton = document.createElement("button");
+   phraseInputButton.innerHTML = 'Submit';
+
+   phraseInputButton.addEventListener('click', () => {
+      if(phraseInput.value === 'kharkov') {
+         initiate();
+      }
+      
+      phraseInput.value = '';
+   })
+
+   bioDiv.appendChild(phraseInput);
+   bioDiv.appendChild(phraseInputButton);
+}
+
+const imgClickCounter = () => {
+   let sum = 0;
+   return () => {
+      if(sum === 5) {
+         sum = 0;
+         validate();
+      }
+      sum+=1;
+   }
+}
+
+profileImgDiv.addEventListener('click', imgClickCounter())
